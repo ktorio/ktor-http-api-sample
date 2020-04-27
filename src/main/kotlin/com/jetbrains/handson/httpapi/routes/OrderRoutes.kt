dac0caeae33e4f59ca0eb7c7c14c633ce53584b0
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-04-27 18:01:09 +0200

    Auto-format code

--- src/main/kotlin/com/jetbrains/handson/httpapi/routes/OrderRoutes.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/OrderRoutes.kt
@@ -1,11 +1,14 @@
 package com.jetbrains.handson.httpapi.routes
 
 import com.jetbrains.handson.httpapi.models.orderStorage
-import io.ktor.application.*
+import io.ktor.application.Application
+import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.response.respondText
-import io.ktor.routing.*
+import io.ktor.routing.Route
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 
 fun Route.listOrdersRoute() {
@@ -18,8 +21,11 @@ fun Route.listOrdersRoute() {
 
 fun Route.getOrderRoute() {
     get("/order/{id}") {
-        val id = call.parameters["id"]  ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
-        val order = orderStorage.find { it.number == id } ?: return@get call.respondText("Not Found", status = HttpStatusCode.NotFound)
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
         call.respond(order)
     }
 }
@@ -27,7 +33,10 @@ fun Route.getOrderRoute() {
 fun Route.totalizeOrderRoute() {
     get("/order/{id}/total") {
         val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
-        val order = orderStorage.find { it.number == id } ?: return@get call.respondText("Not Found", status = HttpStatusCode.NotFound)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
         val total = order.contents.map { it.price * it.amount }.sum()
         call.respondText("Total for order is $total")
     }
