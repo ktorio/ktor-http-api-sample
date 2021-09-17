e02fc4df10ecb381f612e6afb4097223cc03c562
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-09-17 11:35:00 +0300

    update to 2.0.0

--- src/main/kotlin/com/jetbrains/handson/httpapi/routes/OrderRoutes.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/OrderRoutes.kt
@@ -1,10 +1,10 @@
 package com.jetbrains.handson.httpapi.routes
 
 import com.jetbrains.handson.httpapi.models.orderStorage
-import io.ktor.application.*
+import io.ktor.server.application.*
 import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Route.listOrdersRoute() {
     get("/order") {
