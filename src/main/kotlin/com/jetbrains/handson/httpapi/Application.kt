41adf126248a2f8f087b811804483f89dac35403
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-09-17 11:43:14 +0300

    update to 2.0.0

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,6 +1,6 @@
 package com.jetbrains.handson.httpapi
 
-import io.ktor.application.Application
+import io.ktor.server.application.Application
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
