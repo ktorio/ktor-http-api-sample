9288cca01bfa42a904626393f0298858c3b18811
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-04-11 11:29:07 +0300

    update final to 2.0.0

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -3,8 +3,8 @@ package com.jetbrains.handson.httpapi
 import com.jetbrains.handson.httpapi.routes.registerCustomerRoutes
 import com.jetbrains.handson.httpapi.routes.registerOrderRoutes
 import io.ktor.server.application.*
-import io.ktor.server.plugins.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
